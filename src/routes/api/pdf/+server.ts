b99c7c8434b5a3a1d28f5ddcc88a804b0d46f27a
Jaco Kok <kokjaco2@gmail.com>
2024-08-07 15:08:16 +0200

    Trying out cloudflare

--- src/routes/api/pdf/+server.ts
+++ src/routes/api/pdf/+server.ts
@@ -1,6 +1,6 @@
 import type { RequestHandler } from "./$types";
-import { CHROMIUM_PATH } from "$env/static/private";
-import { chromium } from "playwright-core";
+// import { CHROMIUM_PATH } from "$env/static/private";
+// import { chromium } from "playwright-core";
 
 interface Params {
 	userId: string;
@@ -12,29 +12,29 @@ interface Params {
 export const POST: RequestHandler = async ({ request }) => {
 	const params = (await request.json()) as Params;
 	// const result = JSON.stringify(params);
-	const pdf = await htmlToPDF(params);
-	return new Response(pdf);
+	// const pdf = await htmlToPDF(params);
+	return new Response(JSON.stringify(params));
 };
 
-const htmlToPDF = async (params: Params) => {
-	const browser = await chromium.launch({
-		executablePath: CHROMIUM_PATH,
-		// TODO: Remove headless
-		headless: false,
-	});
-	const page = await browser.newPage();
-	await page.emulateMedia({ media: "screen" });
-	await page.emulateMedia({ colorScheme: params.colorScheme });
-	await page.goto(
-		`http://localhost:5173/pdf/${params.userId}/${params.projectId}/${encodeURIComponent(params.date)}`
-	);
-	const result = await page.pdf({
-		// format: "",
-		// preferCSSPageSize: true,
-		// height: "100wh",
-		printBackground: true,
-		// margin: { left: "0px", top: "20px", right: "0px", bottom: "0px" },
-	});
-	await browser.close();
-	return result;
-};
+// const htmlToPDF = async (params: Params) => {
+// 	const browser = await chromium.launch({
+// 		executablePath: CHROMIUM_PATH,
+// 		// TODO: Remove headless
+// 		headless: false,
+// 	});
+// 	const page = await browser.newPage();
+// 	await page.emulateMedia({ media: "screen" });
+// 	await page.emulateMedia({ colorScheme: params.colorScheme });
+// 	await page.goto(
+// 		`http://localhost:5173/pdf/${params.userId}/${params.projectId}/${encodeURIComponent(params.date)}`
+// 	);
+// 	const result = await page.pdf({
+// 		// format: "",
+// 		// preferCSSPageSize: true,
+// 		// height: "100wh",
+// 		printBackground: true,
+// 		// margin: { left: "0px", top: "20px", right: "0px", bottom: "0px" },
+// 	});
+// 	await browser.close();
+// 	return result;
+// };
--- src/routes/api/pw/+server.ts
+++ src/routes/api/pw/+server.ts
@@ -1,24 +1,24 @@
 import type { RequestHandler } from "./$types";
-import { CHROMIUM_PATH } from "$env/static/private";
-import { chromium } from "playwright-core";
+// import { CHROMIUM_PATH } from "$env/static/private";
+// import { chromium } from "playwright-core";
 
 export const GET: RequestHandler = async () => {
-	const result = await htmlToPDF();
-	return new Response(result);
+	// const result = await htmlToPDF();
+	return new Response("w");
 };
 
-const htmlToPDF = async () => {
-	const browser = await chromium.launch({
-		executablePath: CHROMIUM_PATH,
-	});
-	const page = await browser.newPage();
+// const htmlToPDF = async () => {
+// 	const browser = await chromium.launch({
+// 		executablePath: CHROMIUM_PATH,
+// 	});
+// 	const page = await browser.newPage();
 
-	await page.goto("http://localhost:5173/pdf");
-	const result = await page.pdf({
-		format: "A4",
-		printBackground: true,
-		margin: { left: "0px", top: "0px", right: "0px", bottom: "0px" },
-	});
-	await browser.close();
-	return result;
-};
+// 	await page.goto("http://localhost:5173/pdf");
+// 	const result = await page.pdf({
+// 		format: "A4",
+// 		printBackground: true,
+// 		margin: { left: "0px", top: "0px", right: "0px", bottom: "0px" },
+// 	});
+// 	await browser.close();
+// 	return result;
+// };
