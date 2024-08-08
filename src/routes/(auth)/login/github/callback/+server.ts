88ec748128a00e047533cee99500afe8ce23988d
Jaco Kok <kokjaco2@gmail.com>
2024-08-08 10:17:49 +0200

    Trying progress to see where it fails

--- src/routes/(auth)/login/github/callback/+server.ts
+++ src/routes/(auth)/login/github/callback/+server.ts
@@ -13,21 +13,31 @@ export async function GET(event: RequestEvent): Promise<Response> {
 	const storedState = event.cookies.get("github_oauth_state") ?? null;
 
 	if (!code || !state || !storedState || state !== storedState) {
+		console.log(state !== storedState);
 		return json({ code, state, storedState, message: "Need code and state" }, { status: 400 });
 	}
 
+	const progress: Array<string> = [];
+
 	try {
+		progress.push("start");
 		const tokens = await github.validateAuthorizationCode(code);
+		progress.push("tokens " + JSON.stringify(tokens));
 		const githubUserResponse = await fetch("https://api.github.com/user", {
 			headers: {
 				Authorization: `Bearer ${tokens.accessToken}`,
 			},
 		});
+		progress.push("githubUserResponse " + JSON.stringify(githubUserResponse));
 		const githubUser: GitHubUser = await githubUserResponse.json();
+
+		progress.push("githubUser " + JSON.stringify(githubUser));
 
 		const existingUser = await db.query.userTable.findFirst({
 			where: eq(userTable.githubId, githubUser.id.toString()),
 		});
+
+		progress.push("existingUser " + JSON.stringify(existingUser));
 
 		if (existingUser) {
 			const session = await lucia.createSession(existingUser.id, {});
@@ -37,7 +47,9 @@ export async function GET(event: RequestEvent): Promise<Response> {
 				...sessionCookie.attributes,
 			});
 		} else {
+			progress.push("generateIdFromEntropySize");
 			const userId = generateIdFromEntropySize(10); // 16 characters long
+			progress.push("userId " + userId);
 
 			// Replace this with your own DB client.
 			await db.insert(userTable).values({
@@ -66,9 +78,9 @@ export async function GET(event: RequestEvent): Promise<Response> {
 		// the specific error message depends on the provider
 		if (e instanceof OAuth2RequestError) {
 			// invalid code
-			return json({ error: e, message: "Invalid code" }, { status: 400 });
+			return json({ error: e, message: "Invalid code", progress }, { status: 400 });
 		}
-		return json({ error: e, message: "Critical Error" }, { status: 500 });
+		return json({ error: e, message: "Critical Error", progress }, { status: 500 });
 	}
 }
 
